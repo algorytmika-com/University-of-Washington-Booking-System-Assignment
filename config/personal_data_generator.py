7b4e2fd9483dbee09a19368ef7e4f0eb4282ec37
Vector <vector@algorytmika.com>
2023-04-08 18:24:25 +0200

    Personal Data generator with faker refactored

--- config/personal_data_generator.py
+++ config/personal_data_generator.py
@@ -10,15 +10,15 @@ df = pd.DataFrame(
         {
             "booking_id" : fake.unique.random_int(min=1, max=999),
             "customer_name" : fake.name(),
-            "destination_country" : fake.country(),
+            "destination_country" : "United States of America" if fake.boolean(chance_of_getting_true = 50) else fake.country(),
             "destination_city" : fake.city(),
             "package_description" : fake.sentence(nb_words=8),
-            "weight" : fake.pyfloat(positive=True, min_value=0, max_value=10),
-            "volume" : fake.pyfloat(positive=True, min_value=0, max_value=125),
+            "weight" : fake.pyfloat(min_value=0, max_value=10),
+            "volume" : fake.pyfloat(min_value=0, max_value=125),
             "required_delivery_date" : fake.date('%m/%d/%Y'),
             "is_dangerous": fake.boolean(chance_of_getting_true = 20),
             "is_urgent" : fake.boolean(chance_of_getting_true = 30),
-            "price_accepted" : fake.pyfloat(positive=True, min_value=25, max_value=200)
+            "price_accepted" : fake.pyfloat(min_value=25, max_value=200)
         }
         for _ in range(100)
     ]
